f4ae4b1b9921be20f91bef46dbdac188e77cd146
dlxzeus777 <silverchannelcontact@gmail.com>
2023-01-06 17:50:27 +0100

    'changes'

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,24 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const Header = () => {
-  return (
-    <header>
-        <nav>
-            <div className='nav-heading'>
-                <Link to='/' className='logo'><h2>CRYPTO
-                    <span className='font-gradient'>CLOUD</span>
-                </h2></Link>
-            </div>
-            <ul className='nav-list'>
-                <Link className='nav-items' to='/'>Home</Link>
-                <Link className='nav-items' to='/cryptocurrency'>Cryptocurrency</Link>
-                <Link className='nav-items' to='/marketplace'>Marketplace</Link>
-            </ul>
-        </nav>
-    </header>
-  )
+
+    return (
+        <header>
+            <nav>
+                <div className='nav-heading'>
+                    <NavLink to='/' className='logo'><h2>CRYPTO
+                        <span className='font-gradient'>CLOUD</span>
+                    </h2></NavLink>
+                </div>
+                <ul className='nav-list'>
+                    <NavLink className={`nav-items ${({ isActive }) => (isActive ? 'active' : 'inactive')}`} to='/'>Home</NavLink>
+                    <NavLink className={`nav-items ${({ isActive }) => (isActive ? 'active' : 'inactive')}`} to='/cryptocurrency'>Cryptocurrency</NavLink>
+                    <NavLink className={`nav-items ${({ isActive }) => (isActive ? 'active' : 'inactive')}`} to='/marketplace'>Marketplace</NavLink>
+                </ul>
+            </nav>
+        </header>
+    )
 }
 
 export default Header
